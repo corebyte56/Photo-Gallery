b04c118d162bfd0bfb15035cfb80868a15a1c53c
corebyte56 <fahim789m@gmail.com>
2026-03-11 23:50:12 +0600

    Fetch photo data from API and store in state using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [userData, setUserData] = useState(0);
+
+    const  Photogallery = async () => {
+    const res = await fetch('https://picsum.photos/v2/list?page=2&limit=100');
+    let data = await res.json();
+    setUserData(data)
+    console.log(data)
+  
+  }
+
   return (
-    <div>
-      
+    <div className="bg-black h-screen">
+      <button onClick={Photogallery} className='text-white text-4xl font-semibold'>hi</button>
     </div>
   )
 }
